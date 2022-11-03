a006763827a8020c18b163e3ad9832076ce6d432
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-11-03 10:05:44 -0700

    tests passing

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,17 @@
 def oxford_comma(items):
-    return None
+    # if len(items) == 1:
+    #     return items[0]
+    # elif len(items) == 2:
+    #     return " and ".join(items)
+
+    # last_item = items[-1]
+    # oxford_string = ", ".join(items[0:-1])
+    # oxford_string += f", and {last_item}"
+    # return oxford_string
+    if len(items) > 1:
+        items[-1] = f"and {items[-1]}"
+
+    if len(items) > 2:
+        return ", ".join(items)
+    else:
+        return " ".join(items)
